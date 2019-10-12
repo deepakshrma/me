82cc6c84e0051b36ddb3d50d4664b84f0f2cb75a
xdeepakv <deepak.m.shrma@gmail.com>
2019-10-12 17:14:56 +0800

    added i frame

--- src/app/App.kt
+++ src/app/App.kt
@@ -1,10 +1,10 @@
 package app
 
+import kotlinx.html.*
 import react.*
 import react.dom.*
 import logo.*
 import ticker.*
-
 class App : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
         div("App-header") {
@@ -15,8 +15,25 @@ class App : RComponent<RProps, RState>() {
             
         }
         p("App-intro") {
-            +"This website build on Koltin => L0v3 "
+            +"This website build on Koltin"
         }
+        p("App-intro") {
+            +"[<==< WORK IN PROGRESS >==>]"
+        }
+        div("content") {
+            a {
+                attrs {
+                    href = "https://docs.google.com/document/d/1oZ6iGdiALawzS6xlFC_Uwcms04SrD6Wk6NML6CDhxjM/export?format=pdf"
+                    +"Download"
+                }
+            }
+            iframe(IframeSandbox.allowSameOrigin, "full__screen__iframe") {
+                attrs {
+                    src = "https://docs.google.com/document/d/e/2PACX-1vTR3ndWxpXUwpZOKL81B46wxNw1CfvVFWE9nzve5gGxz8qqKi8u_jFrxdL-NgDEEPRRjGFDTuL-hQEy/pub?embedded=true"
+                }
+            }
+        }
+
     }
 }
 
